e626789521ecd296dfe703d140c3b130c603a25a
Jung Hyun Lim <bully1004@naver.com>
2023-03-28 10:46:56 +0900

    1차업데이트

---
+++ src/main/java/com/edu/wepet/domain/Sitter.java
@@ -0,0 +1,8 @@
+package com.edu.wepet.domain;
+
+import lombok.Data;
+
+@Data
+public class Sitter {
+	
+}
---
+++ src/main/java/comd/edu/wepet/holiday/HolidayService.java
@@ -0,0 +1,12 @@
+package comd.edu.wepet.holiday;
+
+import java.util.List;
+
+import com.edu.wepet.domain.Holiday;
+
+public interface HolidayService {
+	public void insert(List holidayList);
+	public List selectAll();
+	public Holiday select(int holiday_idx);
+	public void delete(Holiday holiday);
+}
